7daf03a314e98398c03cf5e4fd19840f9cd4369e
maggieliu42 <59812146+maggieliu42@users.noreply.github.com>
2021-12-06 19:03:50 -0600

    Add files via upload

---
+++ microsite/final/scripts/scripts.js
@@ -0,0 +1,10 @@
+{/* <script>
+    $(document ).ready(function() {
+
+        $(".controls").click(function() {
+            $(".drawer").toggleClass("open");
+            console.log("Toggled the class!");
+        });
+
+    });
+</script>  */}
